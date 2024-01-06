01d271b76d15f90a0e661aa5f6cf9f2c17ecc4d3
menymp <lmeny04@hotmail.com>
2024-01-06 00:09:05 -0600

    add esp32 carrier changes, add prove and aditional sensor, improve fsm

--- metalTester_v1/metalTester_v1.ino
+++ metalTester_v1/metalTester_v1.ino
@@ -5,139 +5,249 @@
  *  
  *  menymp
  *  Dic 2023
+ *  
+ *  YYYY/MM/DD  NAME    CHANGE
+ *  2024/01/05  menymp  change logic to add two sensors and prove extension
+ *                      I/O layout change to use ESP32 carrier board
  */
+/* FSM STATES */
 typedef enum {
   READY,
-  SUPERVISOR_INSPECTION,
-  BUSY,
-  DEFECTIVE_STATE,
-  LATCH_ACTIVE
+  LATCH,
+  SET_LOCK,
+  SET_PROVE,
+  TEST,
+  UNLOCK,
+  DEFECT
 }SYSTEM_STATE;
 
+/* INPUTS DEFINITIONS */
 #define METAL_SENSOR_ACTIVE     true
+#define BUTTON_PUSHED           true
+#define SUPERVISOR_ACTIVE       true
+/* OUTPUTS DEFINITIONS */
 #define LAMP_ON                 true
 #define LAMP_OFF                false
-#define CLAMP_LOCKED            true
-#define CLAMP_FREE              false
-#define SUPERVISOR_LOCK_ACTIVE  true
-#define BUTTON_PUSHED           true
+#define SET_LOCK_OUT            true
+#define RESET_LOCK              false
+#define SET_PROVE_OUT           true
+#define RESET_PROVE             false
+/* CLOCK DELAY */
+#define CLOCK_DELAY             100
+/* STATES DELAY COUNTS */
+/* estimated with pneumatic moves since there are no sensors */
+#define LATCH_TIME              10
+#define LOCK_TIME               10
+#define PROVE_TIME              10
+#define TEST_TIME               10
 
-#define TICK_TIME            100
-#define BUSY_TICKS            20
-#define INTERLOCK_MAX_TIME     3
 /*log messages definitions*/
-#define HARD_FAULT_MSG      "HARD_FAULT_ERR"
-#define SUPERVISOR_MSG      "SUPERVISOR_STATE"
-#define DEFECTIVE_STATE_MSG "BUSY_STATE"
-#define READY_STATE_MSG     "READY_STATE"
-#define BUSY_STATE_MSG      "BUSY_STATE_MSG"
-#define LATCH_MSG           "INTERLOCK_REACHED"
+#define READY_MSG      "READY_STATE"
+#define LATCH_MSG      "LATCH_STATE"
+#define LOCK_MSG       "LOCK_STATE"
+#define SET_LOCK_MSG   "SET_LOCK_STATE"
+#define SET_PROVE_MSG  "SET_PROVE_STATE"
+#define TEST_MSG       "TEST_STATE"
+#define DEFECT_MSG     "DEFECT_STATE"
+#define UNLOCK_MSG     "UNLOCK_STATE"
+#define HARD_FAULT_MSG "HARD_FAULT"
 
-//Operator buttons
-const int BUTTON_A_PIN = 12;
-const int BUTTON_B_PIN = 11;
-const int SUPERVISOR_LOCK_PIN = 10;
-const int METAL_SENSOR_PIN = 9;
-//outputs
-const int GREEN_LAMP_PIN = 7;
-const int RED_LAMP_PIN = 6;
-const int CLAMP_PIN = 5;
+/* INPUT PINS */
+#define SENSOR_1_PIN        15
+#define SENSOR_2_PIN        2
+#define BUTTON_A_PIN        4
+#define BUTTON_B_PIN        5
+#define SUPERVISOR_LOCK_PIN 18
 
+/* OUTPUT PINS */
+#define LOCK_PIN        13
+#define PROVE_PIN       12
+#define RED_LAMP_PIN    14
+#define GREEN_LAMP_PIN  27
+
+/* Reset signal counters */
 SYSTEM_STATE state = READY;
-int busy_cnt = 0;
 int latch_cnt = 0;
+int lock_move_cnt = 0;
+int prove_move_cnt = 0;
+int test_wait_cnt = 0;
+
 
 /*init io and log by serial*/
 void setup() {
+  /* INPUT SETUP */
+  pinMode(SENSOR_1_PIN, INPUT_PULLUP);
+  pinMode(SENSOR_2_PIN, INPUT_PULLUP);
   pinMode(BUTTON_A_PIN, INPUT_PULLUP);
   pinMode(BUTTON_B_PIN, INPUT_PULLUP);
   pinMode(SUPERVISOR_LOCK_PIN, INPUT_PULLUP);
-  pinMode(METAL_SENSOR_PIN, INPUT_PULLUP);
-
-  pinMode(GREEN_LAMP_PIN, OUTPUT);
+  /* OUTPUT SETUP */
+  pinMode(LOCK_PIN, OUTPUT);
+  pinMode(PROVE_PIN, OUTPUT);
   pinMode(RED_LAMP_PIN, OUTPUT);
-  pinMode(CLAMP_PIN, OUTPUT);
-  //Start in debugging mode
+  pinMode(GREEN_LAMP_PIN, OUTPUT);
+  /* OUTPUT INITIAL STATES */
+  digitalWrite(LOCK_PIN, RESET_LOCK);
+  digitalWrite(PROVE_PIN, RESET_PROVE);
+  digitalWrite(RED_LAMP_PIN, LAMP_OFF);
+  digitalWrite(GREEN_LAMP_PIN, LAMP_OFF);
+  /* SERIAL LOG */
   Serial.begin(9600);
 }
 
 /*main state machine*/
 void loop() {
+  int button_a = digitalRead(BUTTON_A_PIN);
+  int button_b = digitalRead(BUTTON_B_PIN);
+  int sensor_1 = digitalRead(SENSOR_1_PIN);
+  int sensor_2 = digitalRead(SENSOR_2_PIN);
+  int supervisor_lock = digitalRead(SUPERVISOR_LOCK_PIN);
+  
   switch(state){
     case READY:
-      if(digitalRead(SUPERVISOR_LOCK_PIN) == SUPERVISOR_LOCK_ACTIVE){
-        state = SUPERVISOR_INSPECTION;
+      if (supervisor_lock != SUPERVISOR_ACTIVE && (button_a == BUTTON_PUSHED || button_b == BUTTON_PUSHED)) {
+        state = LATCH;
       }
-      if(digitalRead(BUTTON_A_PIN) != digitalRead(BUTTON_B_PIN)){
-        latch_cnt++;
-      }
-      if(latch_cnt > INTERLOCK_MAX_TIME){
-        /*TIMEOUT reached, going to latch*/
-        state = LATCH_ACTIVE;
-      }else if (digitalRead(BUTTON_A_PIN) == BUTTON_PUSHED && digitalRead(BUTTON_B_PIN) == BUTTON_PUSHED) {
-        state = BUSY;
-      }
-      digitalWrite(GREEN_LAMP_PIN, LAMP_ON);
+      latch_cnt = 0;
+      lock_move_cnt = 0;
+      prove_move_cnt = 0;
+      test_wait_cnt = 0;
+      
+      digitalWrite(LOCK_PIN, RESET_LOCK);
+      digitalWrite(PROVE_PIN, RESET_PROVE);
       digitalWrite(RED_LAMP_PIN, LAMP_OFF);
-      digitalWrite(CLAMP_PIN, CLAMP_FREE);
-      Serial.println(READY_STATE_MSG);
+      digitalWrite(GREEN_LAMP_PIN, LAMP_ON);
+      Serial.println(READY_MSG);
       break;
       
-    case LATCH_ACTIVE:
-      if(digitalRead(BUTTON_A_PIN) != BUTTON_PUSHED && digitalRead(BUTTON_B_PIN) != BUTTON_PUSHED){
+    case LATCH:
+      if (supervisor_lock == SUPERVISOR_ACTIVE || (button_a != BUTTON_PUSHED || button_b != BUTTON_PUSHED)) {
         state = READY;
-        latch_cnt = 0;
       }
-      digitalWrite(GREEN_LAMP_PIN, LAMP_OFF);
-      digitalWrite(RED_LAMP_PIN, !digitalRead(RED_LAMP_PIN));
-      digitalWrite(CLAMP_PIN, CLAMP_FREE);
+      if (supervisor_lock != SUPERVISOR_ACTIVE && button_a == BUTTON_PUSHED && button_b == BUTTON_PUSHED && latch_cnt < LATCH_TIME) {
+        state = SET_LOCK;
+      }
+      if (latch_cnt < LATCH_TIME) latch_cnt ++;
+      lock_move_cnt = 0;
+      prove_move_cnt = 0;
+      test_wait_cnt = 0;
+      
+      digitalWrite(LOCK_PIN, RESET_LOCK);
+      digitalWrite(PROVE_PIN, RESET_PROVE);
+      digitalWrite(RED_LAMP_PIN, LAMP_OFF);
+      digitalWrite(GREEN_LAMP_PIN, LAMP_ON);
       Serial.println(LATCH_MSG);
       break;
       
-    case SUPERVISOR_INSPECTION:
-      /*in this state, system waits for the supervisor to remove the key*/
-      if(digitalRead(SUPERVISOR_LOCK_PIN) != SUPERVISOR_LOCK_ACTIVE){
-        state = SUPERVISOR_INSPECTION;
-      }
-      digitalWrite(GREEN_LAMP_PIN, !digitalRead(GREEN_LAMP_PIN));
-      digitalWrite(RED_LAMP_PIN, LAMP_ON);
-      digitalWrite(CLAMP_PIN, CLAMP_FREE);
-      Serial.println(SUPERVISOR_MSG);
-      break;
-      
-    case BUSY:
-      if(digitalRead(SUPERVISOR_LOCK_PIN) == SUPERVISOR_LOCK_ACTIVE){
-        state = SUPERVISOR_INSPECTION;
-      }
-      if (digitalRead(METAL_SENSOR_PIN) != METAL_SENSOR_ACTIVE){
-        state = DEFECTIVE_STATE;
-      } else if (busy_cnt > BUSY_TICKS) {
+    case SET_LOCK:
+      if (supervisor_lock == SUPERVISOR_ACTIVE || (button_a != BUTTON_PUSHED || button_b != BUTTON_PUSHED)) {
         state = READY;
-      } else {
-        busy_cnt ++;
       }
-      digitalWrite(GREEN_LAMP_PIN, !digitalRead(GREEN_LAMP_PIN));
+      if (supervisor_lock != SUPERVISOR_ACTIVE && button_a == BUTTON_PUSHED && button_b == BUTTON_PUSHED && lock_move_cnt == LOCK_TIME) {
+        state = SET_PROVE;
+      }
+      
+      latch_cnt = 0;
+      if (lock_move_cnt < LOCK_TIME) lock_move_cnt ++;
+      prove_move_cnt = 0;
+      test_wait_cnt = 0;
+      
+      digitalWrite(LOCK_PIN, SET_LOCK_OUT);
+      digitalWrite(PROVE_PIN, RESET_PROVE);
       digitalWrite(RED_LAMP_PIN, LAMP_OFF);
-      digitalWrite(CLAMP_PIN, CLAMP_LOCKED);
-      Serial.println(BUSY_STATE_MSG);
+      digitalWrite(GREEN_LAMP_PIN, LAMP_OFF);
+      Serial.println(SET_LOCK_MSG);
       break;
       
-    case DEFECTIVE_STATE:
-      if(digitalRead(SUPERVISOR_LOCK_PIN) == SUPERVISOR_LOCK_ACTIVE){
-        state = SUPERVISOR_INSPECTION;
+    case SET_PROVE:
+      if (supervisor_lock == SUPERVISOR_ACTIVE || (button_a != BUTTON_PUSHED || button_b != BUTTON_PUSHED)) {
+        state = UNLOCK;
       }
+      if (supervisor_lock != SUPERVISOR_ACTIVE && button_a == BUTTON_PUSHED && button_b == BUTTON_PUSHED && prove_move_cnt == PROVE_TIME) {
+        state = TEST;
+      }
+      
+      latch_cnt = 0;
+      lock_move_cnt = 0;
+      if (prove_move_cnt < PROVE_TIME) prove_move_cnt ++;
+      test_wait_cnt = 0;
+      
+      digitalWrite(LOCK_PIN, SET_LOCK_OUT);
+      digitalWrite(PROVE_PIN, SET_PROVE_OUT);
+      digitalWrite(RED_LAMP_PIN, LAMP_OFF);
       digitalWrite(GREEN_LAMP_PIN, LAMP_OFF);
+      Serial.println(SET_PROVE_MSG);
+      break;
+      
+    case TEST:
+      if (supervisor_lock == SUPERVISOR_ACTIVE && (button_a == BUTTON_PUSHED && button_b == BUTTON_PUSHED)) {
+        state = UNLOCK;
+      }
+      if ((sensor_1 != METAL_SENSOR_ACTIVE || sensor_2 != METAL_SENSOR_ACTIVE) && prove_move_cnt  >= PROVE_TIME) {
+        state = DEFECT;
+      }
+      if (sensor_1 == METAL_SENSOR_ACTIVE && sensor_2 == METAL_SENSOR_ACTIVE && prove_move_cnt  >= PROVE_TIME) {
+        state = UNLOCK;
+      }
+      
+      latch_cnt = 0;
+      lock_move_cnt = 0;
+      prove_move_cnt = 0;
+      if (test_wait_cnt < TEST_TIME) test_wait_cnt ++;
+      
+      digitalWrite(LOCK_PIN, SET_LOCK_OUT);
+      digitalWrite(PROVE_PIN, SET_PROVE_OUT);
+      digitalWrite(RED_LAMP_PIN, LAMP_OFF);
+      digitalWrite(GREEN_LAMP_PIN, LAMP_OFF);
+      Serial.println(TEST_MSG);
+      break;
+      
+    case UNLOCK:
+      if (button_a == BUTTON_PUSHED && button_b == BUTTON_PUSHED) {
+        state = READY;
+      }
+
+      latch_cnt = 0;
+      if (lock_move_cnt < LOCK_TIME) lock_move_cnt ++;
+      prove_move_cnt = 0;
+      test_wait_cnt = 0;
+
+      digitalWrite(LOCK_PIN, SET_LOCK_OUT);
+      digitalWrite(PROVE_PIN, RESET_PROVE);
+      digitalWrite(RED_LAMP_PIN, LAMP_OFF);
+      digitalWrite(GREEN_LAMP_PIN, LAMP_ON);
+      Serial.println(UNLOCK_MSG);
+      break;
+      
+    case DEFECT:
+      if (supervisor_lock == SUPERVISOR_ACTIVE && (button_a == BUTTON_PUSHED && button_b == BUTTON_PUSHED)) {
+        state = UNLOCK;
+      }
+
+      latch_cnt = 0;
+      lock_move_cnt = 0;
+      prove_move_cnt = 0;
+      test_wait_cnt = 0;
+
+      digitalWrite(LOCK_PIN, SET_LOCK_OUT);
+      digitalWrite(PROVE_PIN, RESET_PROVE);
       digitalWrite(RED_LAMP_PIN, LAMP_ON);
-      digitalWrite(CLAMP_PIN, CLAMP_LOCKED);
-      Serial.println(DEFECTIVE_STATE_MSG);
+      digitalWrite(GREEN_LAMP_PIN, LAMP_OFF);
+      Serial.println(DEFECT_MSG);
       break;
       
     default:
-      digitalWrite(GREEN_LAMP_PIN, LAMP_OFF);
-      digitalWrite(RED_LAMP_PIN, LAMP_OFF);
-      digitalWrite(CLAMP_PIN, CLAMP_FREE);
+      latch_cnt = 0;
+      lock_move_cnt = 0;
+      prove_move_cnt = 0;
+      test_wait_cnt = 0;
+      
+      digitalWrite(LOCK_PIN, RESET_LOCK);
+      digitalWrite(PROVE_PIN, RESET_PROVE);
+      digitalWrite(RED_LAMP_PIN, LAMP_ON);
+      digitalWrite(GREEN_LAMP_PIN, LAMP_ON);
       Serial.println(HARD_FAULT_MSG);
       break;
   }
-  delay(TICK_TIME);
+  delay(CLOCK_DELAY);
 }
